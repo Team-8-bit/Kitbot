dfd27cb0517fa34e4ab552be95896d8087467ee3
brudderbot <brudderbot@outlook.com>
2024-08-04 14:12:21 -0700

    Format Commands better

--- robot/src/main/kotlin/org/team9432/Controls.kt
+++ robot/src/main/kotlin/org/team9432/Controls.kt
@@ -3,10 +3,7 @@ package org.team9432
 
 import org.team9432.commands.driveTrain.slowDrive
 import org.team9432.commands.driveTrain.teleDrive
-import org.team9432.commands.shooter.intake
-import org.team9432.commands.shooter.shoot
-import org.team9432.commands.shooter.shootAmp
-import org.team9432.commands.shooter.shootEnd
+import org.team9432.commands.shooter.*
 import org.team9432.lib.commandbased.commands.SequentialCommand
 import org.team9432.lib.commandbased.input.KXboxController
 import org.team9432.subsystems.Drivetrain
@@ -17,13 +14,10 @@ object Controls {
 
     init {
         Drivetrain.defaultCommand = teleDrive({ controller.getRawAxis(1) * .75 }, { controller.getRawAxis(0) * .75 })
-
         controller.leftBumper.whileTrue(slowDrive({ controller.getRawAxis(1) }, { controller.getRawAxis(0) }))
 
-        controller.a.onTrue(shoot())
-        controller.a.onFalse(shootEnd())
         controller.b.onTrue(intake())
-        controller.b.onFalse(SequentialCommand(Shooter.Commands.setTopSpeed(0.0), Shooter.Commands.setBottomSpeed(0.0)))
         controller.x.onTrue(shootAmp())
+        controller.y.whileTrue(shoot())
     }
 }
--- robot/src/main/kotlin/org/team9432/commands/shooter/intake.kt
+++ robot/src/main/kotlin/org/team9432/commands/shooter/intake.kt
@@ -1,19 +1,31 @@
 package org.team9432.commands.shooter
 
 import org.team9432.lib.commandbased.commands.SequentialCommand
+import org.team9432.lib.commandbased.commands.SimpleCommand
 import org.team9432.lib.commandbased.commands.WaitCommand
 import org.team9432.lib.commandbased.commands.WaitUntilCommand
 import org.team9432.subsystems.Shooter
 
-
-fun intake() = SequentialCommand(
-    Shooter.Commands.setTopSpeed(-1700.0),
-    Shooter.Commands.setBottomSpeed(-1700.0),
-    Shooter.Commands.setSideSpeed(-1700.0),
-    WaitCommand(0.1),
-    WaitUntilCommand(({ Shooter.motorBottom.outputCurrent > 8 })),
-    Shooter.Commands.setBottomSpeed(0.0),
-    Shooter.Commands.setTopSpeed(0.0),
-    Shooter.Commands.setSideSpeed(0.0),
-    Shooter.Commands.setNoteInRobot(true)
+fun intake() = SimpleCommand(
+    requirements = setOf(Shooter),
+    execute = {
+        SequentialCommand(
+            Shooter.Commands.setTopSpeed(-1700.0),
+            Shooter.Commands.setBottomSpeed(-1700.0),
+            Shooter.Commands.setSideSpeed(-1700.0),
+            WaitCommand(0.1),
+            WaitUntilCommand(({ Shooter.motorBottom.outputCurrent > 8 })),
+            Shooter.Commands.setBottomSpeed(0.0),
+            Shooter.Commands.setTopSpeed(0.0),
+            Shooter.Commands.setSideSpeed(0.0),
+            Shooter.Commands.setNoteInRobot(true)
+        ).schedule()
+    },
+    end = {
+        SequentialCommand(
+            Shooter.Commands.setBottomSpeed(0.0),
+            Shooter.Commands.setTopSpeed(0.0),
+            Shooter.Commands.setSideSpeed(0.0)
+        ).schedule()
+    }
 )
